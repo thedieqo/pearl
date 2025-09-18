f1cb7968e6d305c5c34e91b4c1711b7beb021d98
Diegooo <diegonangumaw@gmail.com>
2025-09-17 20:59:20 -0500

    feat: agregar serializers y endpoints API para clientes, inventario y ventas

--- joyeria/urls.py
+++ joyeria/urls.py
@@ -1,23 +1,17 @@
-"""
-URL configuration for joyeria project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from inventario.views import ProductoViewSet
+from clientes.views import ClienteViewSet   # 👈 aquí con minúscula
+from ventas.views import VentaViewSet       # 👈 esta ya estaba bien
+
+router = DefaultRouter()
+router.register(r'productos', ProductoViewSet)
+router.register(r'clientes', ClienteViewSet)
+router.register(r'ventas', VentaViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Venta
+from .serializers import VentaSerializer
+
+class VentaViewSet(viewsets.ModelViewSet):
+    queryset = Venta.objects.all()
+    serializer_class = VentaSerializer
